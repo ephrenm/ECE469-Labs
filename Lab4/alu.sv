dde3785d6a32687d909a06dc6d5de0b8ba61629d
Jose L. Garcia <chewis0823@gmail.com>
2026-02-19 16:42:37 -0600

    Update alu.sv
    added 4-1 Mux code and added comments for what is missing

--- Lab4/alu.sv
+++ Lab4/alu.sv
@@ -6,15 +6,30 @@ module alu(
   output logic zero
 );
   
-  logic [31:0] invB, BB, adder, or1, and1, zext;
+  logic [31:0] invB, BB, adder_out, or_out, and_out, zext_out;
   
   assign invB=~B;
   assign BB=(F[2]) ? invB : B;
 
-  assign or1 = BB | A;
-  assign and1 = BB & A;
-  assign 
+  assign or_out = BB | A;
+  assign and_out = BB & A;
+  //needs adder/subtractor
   
+  //needs zero extender
+
+  always_comb
+    begin
+      case(F[1:0])
+        2'b00: Y=and_out;
+        2'b01: Y=or_out;
+        2'b10: Y=S;
+        2'b11: Y=slt_res;
+        default: Y=32'b0;
+      endcase
+    end
+endmodule
+
+    
   
   
   
