29cb218481c45b84d4d21e8f054c9f53607dac80
ephrenm <ephrenmanning@gmail.com>
2026-04-11 12:03:52 -0500

    Initial top and testbench

---
+++ Lab 8/testbench.sv
@@ -0,0 +1,43 @@
+module testbench;
+    logic reset;
+    logic clk;
+    logic d; 
+
+    logic rec_C, rec_E, rec_4, rec_6, rec_9; [cite: 66]
+
+    top dut (
+        .reset(reset),
+        .clk(clk),
+        .d(d),
+        .rec_C(rec_C),
+        .rec_E(rec_E),
+        .rec_4(rec_4),
+        .rec_6(rec_6),
+        .rec_9(rec_9)
+    );
+
+    always #5 clk = ~clk;
+
+    logic [0:19] test_seq = 20'b01100011110011011101; 
+    integer i;
+
+    initial begin
+        clk = 0;
+        reset = 1;
+        d = 0;
+
+        #15;
+        reset = 0;
+
+        for (i = 0; i < 20; i = i + 1) begin
+            @(posedge clk);
+            d = test_seq[i];
+        end
+
+        #20;
+        
+        $display("Example testbench simulation complete.");
+        $stop; 
+    end
+
+endmodule
---
+++ Lab 8/top.sv
@@ -0,0 +1,76 @@
+module top (
+	input logic 	reset, clk, d,
+	
+	output logic 	rec_C, rec_E, rec_4, rec_6, rec_9
+);
+
+	logic [3:0] state;
+	logic [3:0] next_state;
+
+	always_comb begin
+		//next state logic:
+		next_state = next_state[2:0], d;
+		
+		//output logic:				
+		case (state)
+			4'b1100: begin
+				rec_C = 1'b1; 
+				rec_6 = 1'b0; 
+				rec_E = 1'b0; 
+				rec_4 = 1'b1; 
+				rec_9 = 1'b0;
+				end
+			
+			4'b0100: begin
+				rec_C = 1'b0; 
+				rec_6 = 1'b0; 
+				rec_E = 1'b0; 
+				rec_4 = 1'b1; 
+				rec_9 = 1'b0;
+				end
+				
+			4'b1110: begin
+				rec_C = 1'b0; 
+				rec_6 = 1'b1; 
+				rec_E = 1'b1; 
+				rec_4 = 1'b0; 
+				rec_9 = 1'b0;
+				end
+				
+			4'b0110: begin
+				rec_C = 1'b0; 
+				rec_6 = 1'b1; 
+				rec_E = 1'b0; 
+				rec_4 = 1'b0; 
+				rec_9 = 1'b0;
+				end
+				
+			4'b1001: begin
+				rec_C = 1'b0; 
+				rec_6 = 1'b0; 
+				rec_E = 1'b0; 
+				rec_4 = 1'b0; 
+				rec_9 = 1'b1;
+				end
+				
+			default: begin
+				rec_C = 1'b0; 
+				rec_6 = 1'b0; 
+				rec_E = 1'b0; 
+				rec_4 = 1'b0; 
+				rec_9 = 1'b0;
+				end
+				
+		endcase
+	end
+	
+	always_ff @(posedge clk) begin
+		//state FF:
+		if(reset) begin
+			state <= 4'h0;
+		end else begin
+			state <= next_state;
+		end
+	end
+
+endmodule
